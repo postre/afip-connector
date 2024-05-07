31f902036e2662f702f69ec07407cfaf8b4e24c6
postre <jrodriguezgbk@gmail.com>
2024-05-07 02:50:25 -0300

    licence

--- AfipConnector/Models/Model.php
+++ AfipConnector/Models/Model.php
@@ -1,13 +1,34 @@
 <?php 
+// Copyright (C) 2024 Javier Rodriguez (Postre)
+// 
+// This file is part of AFIP-CONNECTOR.
+// 
+// AFIP-CONNECTOR is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// AFIP-CONNECTOR is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with AFIP-CONNECTOR.  If not, see LICENCE.txt <https://www.gnu.org/licenses/gpl-3.0.html>.
+
+// Limitación de Responsabilidad:
+// EN NINGÚN CASO, EL AUTOR O TITULAR DE LOS DERECHOS DE AUTOR SERÁ RESPONSABLE 
+// ANTE NINGUNA PARTE POR DAÑOS DIRECTOS, INDIRECTOS, INCIDENTALES, ESPECIALES, 
+// EJEMPLARES O CONSECUENTES (INCLUYENDO, PERO NO LIMITADO A, LA ADQUISICIÓN DE 
+// BIENES O SERVICIOS SUSTITUTOS, LA PÉRDIDA DE USO, LOS DATOS O LAS UTILIDADES,
+// LA INTERRUPCIÓN DEL NEGOCIO, LA PÉRDIDA DE BENEFICIOS O LA INFORMACIÓN COMERCIAL), 
+// SIN IMPORTAR LA FORMA DE LA ACCIÓN, YA SEA POR CONTRATO, AGRAVIO (INCLUYENDO 
+// NEGLIGENCIA) O DE OTRO MODO, QUE SURJA DE O EN RELACIÓN CON EL USO O EL RENDIMIENTO 
+// DE ESTE SOFTWARE, INCLUSO SI SE HA ADVERTIDO DE LA POSIBILIDAD DE TALES DAÑOS.
+
+
 namespace AfipConnector\Models;
 
-
-/**
- * IVA
- * 
- * Detalle de alícuotas relacionadas con el comprobante que se solicita autorizar (array).
- * 
- */
 class Model{
     protected function excludedVars():array{
         return [];
--- examples/wsfev1/FEParamGetTiposCbte.php
+++ examples/wsfev1/FEParamGetTiposCbte.php
@@ -1,4 +1,21 @@
 <?php 
+// Copyright (C) 2024 Javier Rodriguez (Postre)
+// 
+// This file is part of AFIP-CONNECTOR.
+// 
+// AFIP-CONNECTOR is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// AFIP-CONNECTOR is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with AFIP-CONNECTOR.  If not, see LICENCE.txt <https://www.gnu.org/licenses/gpl-3.0.html>.
+
 use AfipConnector\Afip\Wsfev1;
 
 require '..\..\AfipConnector\autoload.php';
